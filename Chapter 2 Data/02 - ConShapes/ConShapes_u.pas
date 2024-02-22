96f18c210062d332aaeea1f6032fbbb7189e2e6a
Francois Malan <78906010+Sindrei@users.noreply.github.com>
2024-02-22 16:17:15 +0200

    Completed Consolidation 2.1.2

--- Chapter 2 Data/02 - ConShapes/ConShapes_u.pas
+++ Chapter 2 Data/02 - ConShapes/ConShapes_u.pas
@@ -12,6 +12,9 @@ type
     btnPattern1: TButton;
     btnPattern2: TButton;
     btnPattern3: TButton;
+    procedure btnPattern1Click(Sender: TObject);
+    procedure btnPattern2Click(Sender: TObject);
+    procedure btnPattern3Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,5 +27,66 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TfrmShapes.btnPattern1Click(Sender: TObject);
+var
+  i, j: Integer;
+  sLine: String;
+begin
+  for i := 1 to 4 do
+  begin
+    sLine := '';
+    for j := i to 4 do
+    begin
+      sLine := sLine + inttostr(j);
+    end;
+    memDisplay.Lines.Add(sLine);
+  end;
+end;
+
+procedure TfrmShapes.btnPattern2Click(Sender: TObject);
+var
+  i, j: Integer;
+  sLine: String;
+begin
+  for i := 1 to 5 do
+  begin
+    sLine := '';
+
+    if i MOD 2 = 1 then
+    begin
+      for j := 1 to 6 do
+      begin
+        sLine := sLine + '* ';
+      end;
+    end
+    else
+    begin
+      for j := 1 to 5 do
+      begin
+        sLine := sLine + ' *';
+      end;
+    end;
+    memDisplay.Lines.Add(sLine);
+    memDisplay.Lines.Add('');
+  end;
+end;
+
+procedure TfrmShapes.btnPattern3Click(Sender: TObject);
+var
+  i, j: Integer;
+  sLine: String;
+begin
+  for i := 1 to 6 do
+  begin
+    sLine := '';
+    for j := 1 to 6 - i do
+    begin
+      sLine := sLine + ' ';
+    end;
+    memDisplay.Lines.Add(sLine);
+  end;
+
+end;
 
 end.
